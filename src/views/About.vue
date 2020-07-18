668b98071a6a6269a6b8c245fe2b51d6d172647c
yesid <martinezcorreayesid@gmail.com>
2020-07-18 16:15:32 -0500

    primer commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-for="post of posts"  v-bind:key="post.id">
+      <h5>{{post.title}}</h5>
+      <p>{{post.body}}</p>
+
+    </div>
   </div>
 </template>
+
+<script>
+const URI = 'https://jsonplaceholder.typicode.com/posts';
+
+
+export default {
+  
+  data () {
+    return {
+      posts: []
+    }
+  },
+
+
+  created() {
+    this.getPosts();
+  },
+
+  methods: {
+    async getPosts() {
+      const response  = await fetch(URI);
+      const data = await response.json();
+      this.posts = data;
+    }
+  }
+  
+}
+</script>
